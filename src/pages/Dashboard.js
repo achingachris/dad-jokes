55c0275cf42cbfc3b6ec5f7ad46d8d400b58234e
achinga.chris <chris.achinga@student.moringaschool.com>
2022-12-02 03:11:46 +0300

    dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,11 +1,15 @@
 import PageHeader from '../components/shared/PageHeader'
 import SummaryCardSection from '../components/dashboard/SummaryCardSection'
+import EventsSection from '../components/dashboard/EventsSection'
+import BookingsSection from '../components/dashboard/BookingsSection'
 
 const Dashboard = () => {
   return (
     <div>
       <PageHeader pageName='Dashboard' />
       <SummaryCardSection />
+      <EventsSection />
+      <BookingsSection />
     </div>
   )
 }
