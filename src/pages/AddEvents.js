4b4557d16135da2befdb5898f85143b9c4ec4617
doghot <achinga.chris@gmail.com>
2023-05-25 16:53:43 +0300

    clean

--- src/pages/AddEvents.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const AddEvents = () => {
-  return (
-    <div>AddEvents</div>
-  )
-}
-
-export default AddEvents
--- src/pages/TicketSales.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const TicketSales = () => {
-  return (
-    <div>TicketSales</div>
-  )
-}
-
-export default TicketSales
